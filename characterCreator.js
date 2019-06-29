5c40e3f5579e9956a2721e35b04bca7f0b937ed2
Ian Feekes <ianfeekes@Ians-MacBook-Pro.local>
2019-06-29 16:42:20 -0700

    added forms

--- characterCreator.js
+++ characterCreator.js
@@ -23,15 +23,37 @@ class Character{
 
 var races =  ["human", "halfling", "gnome", "half-orc", "dwarf", "half-Elf", "elf"]; 
 
+var raceDescriptions = "Humans have no ability adjustments but get an extra feat (perk) \n Dwarfs are extra hardy and tend to be a little uggo/unlikeable \nElves have great hands (tossin) but are not super healthy \nGnomes get that extra constitution but no strength. Great for not being a fighter while having strong fighting stats \nHalf elves have a couple skills boosts, and are generally liked by both humans and elves. Or hated by both. \nHalf orcs (you can be a full orc and I won't tell anyone) get extra strength and less intelligence \n Halflings get less dexterity and extra strength\n";
+
+var prompts = ["Enter the name of your new adventurer: ",
+"Now to select your gender (Non-spectrum):",
+"Now to select your race: ",
+];
+
 function main()
 {
     //Creates the textbox from javascript for easier manipulation and scaling 
     var textBox = document.createElement("P"); 
     textBox.setAttribute("id", "textBox");
     document.body.appendChild(textBox);
+    
+    //Creates the form 
+    var inputForm = document.createElement("FORM"); 
+    inputForm.setAttribute("id", "inputForm"); 
+    document.body.appendChild(inputForm); 
+
+    var inputField = document.createElement("INPUT");
+    inputField.setAttribute("type", "text");
+    inputField.setAttribute("value", "");
+    document.getElementById("inputForm").appendChild(inputField);
 
     console.log("Displaying message"); 
     sendTextToHTML("this is some dummy text sent from javascript", "textBox"); 
+
+    for(let i=0;i<prompts.length;++i)
+    {
+        sendTextToHTML(prompts[i], "textBox"); 
+    }
 }
 
 
